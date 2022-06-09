13ea4bf9fd91d8523a5bae4db3afbd1277316512
Tredy01 <tredyworks@gmail.com>
2022-06-09 17:28:47 +0300

    Çalışır durumda klasik galeri scripti.
    Discord : Trédy†#7422

---
+++ esx_vehicleshop/locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+  ['press_to_open'] = "PRESS ~r~E~w~ TO OPEN VEHICLE SHOP",
+  ['enough_money'] = 'Yeterli Paran Yok.',
+  ['we_dont_vehicle'] = 'Bu Araca Sahip Değiliz Lütfen Galerici İle İletişime Geçiniz.',
+  ['car_dealer'] = 'Galerici',
+  ['rotate_keys'] = 'Aracı sola çevirmek için A sağa çevirmek için D basınız.',
+  ['testdrive'] = 'Test Süresinde Kalan Zaman :  '
+ }
